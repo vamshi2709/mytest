f3aab1500700c0ed22945d2479af68d3662bfb0b
vamshi2709 <55499662+vamshi2709@users.noreply.github.com>
2023-07-10 17:24:44 -0700

    Create import time.py

---
+++ import time.py
@@ -0,0 +1,25 @@
+import time
+
+
+def add1(a, b):
+    return a+b
+
+def add(*args):
+    print(args)
+    sum = 0
+    for i in args:
+        sum = sum + i
+    return sum
+
+def names(**kargs):
+    name = kargs.get('name', 'default')
+    return name
+    
+names(name ='Yeshwanth', test = 123)
+
+names()
+
+# a = add(1,2)
+# b = add(2,3,4,5,6,7)
+
+# print(a,b)
